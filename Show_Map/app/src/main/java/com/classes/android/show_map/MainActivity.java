95b958160cd6bddf38c3f6c87afd346bfc23ae71
Nalinee Choudhary <nalineechoudhary@gmail.com>
2017-01-23 14:19:19 +0530

    commit

---
+++ Show_Map/app/src/main/java/com/classes/android/show_map/MainActivity.java
@@ -0,0 +1,35 @@
+package com.classes.android.show_map;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap map;
+    static final LatLng Nalinee = new LatLng(12.555, 77.45);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        if(map!=null){
+            Marker nalinee =map.addMarker(new MarkerOptions().position(Nalinee).title("Nalinee"));
+        }
+
+    }
+
+}
